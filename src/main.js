6a53dddf9f3824ec82e36d969b12d13c0dda4b12
Bill Yang <joyang12345@gmail.com>
2023-06-05 00:03:03 -0400

    add functions

---
+++ src/main.js
@@ -0,0 +1,41 @@
+const addButton = document.getElementById("addButton");
+const todoList = document.getElementById("todoList");
+
+function newItem() {
+    const task = prompt();
+
+    const divContainer = document.createElement("div");
+    divContainer.classList.add("form-check");
+
+    const input = document.createElement("input");
+    input.classList.add("form-check-input");
+    input.setAttribute("type", "checkbox");
+
+    const label = document.createElement("label");
+    label.classList.add("form-check-label");
+    label.textContent = task;
+
+    const split = document.createElement("span");
+    split.textContent = " | ";
+
+    const timeDisplay = document.createElement("span")
+    timeDisplay.textContent = "00:00 ";
+
+    const timerButton = document.createElement("button");
+    timerButton.setAttribute("type", "button");
+    timerButton.classList.add("btn", "btn-success", "btn-sm");
+    timerButton.style.cssText = "--bs-btn-padding-y: .5rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: 1rem;";
+
+    divContainer.appendChild(input);
+    divContainer.appendChild(label);
+    divContainer.appendChild(split);
+    divContainer.appendChild(timeDisplay);
+    divContainer.appendChild(timerButton);
+
+    todoList.appendChild(divContainer);
+}
+
+// To get rid of the check we need to had a listener to each one.
+// So when a new item is created we need to add a new eventListener.
+
+addButton.addEventListener("click", newItem);
