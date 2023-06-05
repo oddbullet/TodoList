906d92093b0d20fc033362d13d553ad7df309a21
Bill Yang <joyang12345@gmail.com>
2023-06-05 00:11:09 -0400

    fix empty add issue

--- src/main.js
+++ src/main.js
@@ -1,38 +1,44 @@
 const addButton = document.getElementById("addButton");
 const todoList = document.getElementById("todoList");
 
+function isEmpty(value) {
+    return (value == null || (typeof value === "string" && value.trim().length === 0));
+}
+
 function newItem() {
     const task = prompt();
 
-    const divContainer = document.createElement("div");
-    divContainer.classList.add("form-check");
+    if(!isEmpty(task)) {
+        const divContainer = document.createElement("div");
+        divContainer.classList.add("form-check");
 
-    const input = document.createElement("input");
-    input.classList.add("form-check-input");
-    input.setAttribute("type", "checkbox");
+        const input = document.createElement("input");
+        input.classList.add("form-check-input");
+        input.setAttribute("type", "checkbox");
 
-    const label = document.createElement("label");
-    label.classList.add("form-check-label");
-    label.textContent = task;
+        const label = document.createElement("label");
+        label.classList.add("form-check-label");
+        label.textContent = task;
 
-    const split = document.createElement("span");
-    split.textContent = " | ";
+        const split = document.createElement("span");
+        split.textContent = " | ";
 
-    const timeDisplay = document.createElement("span")
-    timeDisplay.textContent = "00:00 ";
+        const timeDisplay = document.createElement("span")
+        timeDisplay.textContent = "00:00 ";
 
-    const timerButton = document.createElement("button");
-    timerButton.setAttribute("type", "button");
-    timerButton.classList.add("btn", "btn-success", "btn-sm");
-    timerButton.style.cssText = "--bs-btn-padding-y: .5rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: 1rem;";
+        const timerButton = document.createElement("button");
+        timerButton.setAttribute("type", "button");
+        timerButton.classList.add("btn", "btn-success", "btn-sm");
+        timerButton.style.cssText = "--bs-btn-padding-y: .5rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: 1rem;";
 
-    divContainer.appendChild(input);
-    divContainer.appendChild(label);
-    divContainer.appendChild(split);
-    divContainer.appendChild(timeDisplay);
-    divContainer.appendChild(timerButton);
+        divContainer.appendChild(input);
+        divContainer.appendChild(label);
+        divContainer.appendChild(split);
+        divContainer.appendChild(timeDisplay);
+        divContainer.appendChild(timerButton);
 
-    todoList.appendChild(divContainer);
+        todoList.appendChild(divContainer);
+    }
 }
 
 // To get rid of the check we need to had a listener to each one.
